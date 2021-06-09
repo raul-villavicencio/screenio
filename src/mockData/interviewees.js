061fda0cd4cc13e5f3c745756037883991e9e5a1
Raul Villavicencio <raul.villavicencio@zendesk.com>
2021-06-09 15:00:09 -0500

    added mockup data

---
+++ src/mockData/interviewees.js
@@ -0,0 +1,604 @@
+const data = [
+  [
+    "Igor Ashley",
+    "Designer",
+    "2020/11/27",
+    "pending"
+  ],
+  [
+    "Thor Fulton",
+    "Designer",
+    "2022/01/12",
+    "approved"
+  ],
+  [
+    "Walker Stuart",
+    "Jr",
+    "2022/04/03",
+    "pending"
+  ],
+  [
+    "Rajah Mosley",
+    "Jr",
+    "2022/01/04",
+    "approved"
+  ],
+  [
+    "Fuller Holloway",
+    "SSr",
+    "2020/11/04",
+    "pending"
+  ],
+  [
+    "Colby Swanson",
+    "Architect",
+    "2022/03/27",
+    "approved"
+  ],
+  [
+    "Chadwick Bass",
+    "Architect",
+    "2021/06/30",
+    "approved"
+  ],
+  [
+    "Grady Hogan",
+    "Jr Adv",
+    "2021/04/15",
+    "rejected"
+  ],
+  [
+    "Walter Mcfarland",
+    "Designer",
+    "2020/08/16",
+    "approved"
+  ],
+  [
+    "Tarik Beck",
+    "Architect",
+    "2021/04/14",
+    "rejected"
+  ],
+  [
+    "Ferdinand Garcia",
+    "Jr",
+    "2022/03/28",
+    "rejected"
+  ],
+  [
+    "Dante Flores",
+    "Designer",
+    "2021/12/12",
+    "pending"
+  ],
+  [
+    "Gregory Hines",
+    "SSr",
+    "2022/05/21",
+    "pending"
+  ],
+  [
+    "Matthew Mercado",
+    "SSr",
+    "2020/05/04",
+    "rejected"
+  ],
+  [
+    "Curran Bernard",
+    "Jr Adv",
+    "2022/03/25",
+    "rejected"
+  ],
+  [
+    "Shad Watkins",
+    "Jr",
+    "2021/12/01",
+    "rejected"
+  ],
+  [
+    "Keaton Brown",
+    "Designer",
+    "2020/06/05",
+    "rejected"
+  ],
+  [
+    "Nathan Ingram",
+    "Designer",
+    "2021/09/20",
+    "approved"
+  ],
+  [
+    "Hector Holcomb",
+    "Architect",
+    "2021/11/15",
+    "approved"
+  ],
+  [
+    "Logan Hays",
+    "Jr Adv",
+    "2021/11/25",
+    "approved"
+  ],
+  [
+    "Chancellor Fowler",
+    "SSr",
+    "2021/07/23",
+    "rejected"
+  ],
+  [
+    "Edward England",
+    "Jr",
+    "2021/01/01",
+    "pending"
+  ],
+  [
+    "Galvin Sutton",
+    "Architect",
+    "2020/06/21",
+    "approved"
+  ],
+  [
+    "Cruz Swanson",
+    "Jr",
+    "2021/04/02",
+    "approved"
+  ],
+  [
+    "Chadwick Levine",
+    "Designer",
+    "2021/05/11",
+    "rejected"
+  ],
+  [
+    "Amos Mills",
+    "Designer",
+    "2022/03/23",
+    "rejected"
+  ],
+  [
+    "Seth Hewitt",
+    "Jr Adv",
+    "2021/01/07",
+    "rejected"
+  ],
+  [
+    "Charles Moses",
+    "Architect",
+    "2021/11/27",
+    "pending"
+  ],
+  [
+    "Thomas Morgan",
+    "Architect",
+    "2021/06/22",
+    "rejected"
+  ],
+  [
+    "Timothy Schmidt",
+    "SSr",
+    "2020/11/12",
+    "approved"
+  ],
+  [
+    "Oren Norris",
+    "Jr Adv",
+    "2022/02/17",
+    "pending"
+  ],
+  [
+    "Tanek Espinoza",
+    "Jr",
+    "2021/01/19",
+    "approved"
+  ],
+  [
+    "Chase Compton",
+    "Architect",
+    "2021/11/30",
+    "approved"
+  ],
+  [
+    "David Norton",
+    "SSr",
+    "2021/07/03",
+    "pending"
+  ],
+  [
+    "Cadman Rivas",
+    "Architect",
+    "2022/06/08",
+    "rejected"
+  ],
+  [
+    "Callum Barber",
+    "Jr Adv",
+    "2020/11/15",
+    "pending"
+  ],
+  [
+    "Jeremy Stanton",
+    "Designer",
+    "2021/07/06",
+    "approved"
+  ],
+  [
+    "Reece Slater",
+    "SSr",
+    "2020/07/14",
+    "approved"
+  ],
+  [
+    "Chadwick Soto",
+    "Jr Adv",
+    "2020/06/01",
+    "rejected"
+  ],
+  [
+    "Micah Tyson",
+    "Jr Adv",
+    "2021/11/30",
+    "pending"
+  ],
+  [
+    "Fletcher Leonard",
+    "Architect",
+    "2021/05/16",
+    "pending"
+  ],
+  [
+    "Reuben Watkins",
+    "Designer",
+    "2021/11/14",
+    "pending"
+  ],
+  [
+    "Jakeem Estes",
+    "Architect",
+    "2022/04/16",
+    "approved"
+  ],
+  [
+    "Paki Benton",
+    "Jr Adv",
+    "2022/05/30",
+    "pending"
+  ],
+  [
+    "Berk Ryan",
+    "SSr",
+    "2021/08/29",
+    "rejected"
+  ],
+  [
+    "Alvin Browning",
+    "SSr",
+    "2021/01/04",
+    "approved"
+  ],
+  [
+    "Louis Ross",
+    "Designer",
+    "2022/01/31",
+    "rejected"
+  ],
+  [
+    "Patrick Rogers",
+    "Jr Adv",
+    "2020/05/18",
+    "pending"
+  ],
+  [
+    "Basil Leblanc",
+    "Jr Adv",
+    "2021/09/01",
+    "pending"
+  ],
+  [
+    "Hall Gillespie",
+    "Architect",
+    "2022/01/09",
+    "pending"
+  ],
+  [
+    "Owen Kennedy",
+    "Designer",
+    "2021/01/01",
+    "rejected"
+  ],
+  [
+    "Beck Whitehead",
+    "Jr",
+    "2020/09/09",
+    "approved"
+  ],
+  [
+    "Yuli Berg",
+    "Jr",
+    "2020/06/25",
+    "rejected"
+  ],
+  [
+    "Merrill Pate",
+    "Jr Adv",
+    "2022/04/29",
+    "pending"
+  ],
+  [
+    "Amir Perez",
+    "SSr",
+    "2020/07/08",
+    "approved"
+  ],
+  [
+    "Fitzgerald Whitfield",
+    "Jr",
+    "2020/08/28",
+    "approved"
+  ],
+  [
+    "Quinn Beard",
+    "Designer",
+    "2022/04/26",
+    "approved"
+  ],
+  [
+    "Adam Sears",
+    "Jr",
+    "2022/02/14",
+    "rejected"
+  ],
+  [
+    "Brandon Mclaughlin",
+    "Designer",
+    "2021/09/24",
+    "pending"
+  ],
+  [
+    "Aristotle Riley",
+    "Jr",
+    "2021/06/21",
+    "approved"
+  ],
+  [
+    "Chancellor Irwin",
+    "Jr Adv",
+    "2021/02/11",
+    "rejected"
+  ],
+  [
+    "Ivor Armstrong",
+    "Jr Adv",
+    "2020/12/21",
+    "rejected"
+  ],
+  [
+    "Dexter Lawrence",
+    "Architect",
+    "2021/05/01",
+    "pending"
+  ],
+  [
+    "William Good",
+    "SSr",
+    "2021/04/15",
+    "pending"
+  ],
+  [
+    "Hu Patel",
+    "Designer",
+    "2022/02/24",
+    "rejected"
+  ],
+  [
+    "Holmes Hood",
+    "Architect",
+    "2021/01/01",
+    "rejected"
+  ],
+  [
+    "Garrett Rich",
+    "Jr Adv",
+    "2021/11/29",
+    "pending"
+  ],
+  [
+    "Zahir Velazquez",
+    "Architect",
+    "2021/01/30",
+    "rejected"
+  ],
+  [
+    "Coby Bird",
+    "Designer",
+    "2021/02/10",
+    "rejected"
+  ],
+  [
+    "Ryan Murray",
+    "Jr",
+    "2021/10/24",
+    "approved"
+  ],
+  [
+    "Sebastian Pennington",
+    "Jr",
+    "2021/06/14",
+    "pending"
+  ],
+  [
+    "Jack Walter",
+    "Jr Adv",
+    "2020/06/02",
+    "pending"
+  ],
+  [
+    "Reed Cortez",
+    "SSr",
+    "2021/10/20",
+    "rejected"
+  ],
+  [
+    "Sawyer Bishop",
+    "Jr Adv",
+    "2021/11/21",
+    "rejected"
+  ],
+  [
+    "Thomas Harper",
+    "SSr",
+    "2021/05/25",
+    "pending"
+  ],
+  [
+    "Dexter Glover",
+    "SSr",
+    "2022/05/02",
+    "approved"
+  ],
+  [
+    "Marshall Myers",
+    "Jr Adv",
+    "2021/09/27",
+    "pending"
+  ],
+  [
+    "Aladdin Waller",
+    "Jr Adv",
+    "2021/07/13",
+    "approved"
+  ],
+  [
+    "Rashad Graham",
+    "Jr Adv",
+    "2021/06/14",
+    "approved"
+  ],
+  [
+    "Adrian Hobbs",
+    "Architect",
+    "2021/03/28",
+    "pending"
+  ],
+  [
+    "Stewart Johnson",
+    "Designer",
+    "2021/11/26",
+    "rejected"
+  ],
+  [
+    "Henry King",
+    "Jr Adv",
+    "2021/08/05",
+    "approved"
+  ],
+  [
+    "Bernard Rocha",
+    "Designer",
+    "2020/12/21",
+    "rejected"
+  ],
+  [
+    "Gil Blackwell",
+    "Architect",
+    "2020/05/22",
+    "rejected"
+  ],
+  [
+    "Rafael Small",
+    "Architect",
+    "2021/06/08",
+    "pending"
+  ],
+  [
+    "Stewart Walls",
+    "Jr",
+    "2022/05/31",
+    "pending"
+  ],
+  [
+    "Isaiah Simmons",
+    "Jr",
+    "2022/04/16",
+    "pending"
+  ],
+  [
+    "Gil Oneill",
+    "Jr",
+    "2021/03/11",
+    "pending"
+  ],
+  [
+    "Akeem Carter",
+    "Jr Adv",
+    "2020/07/03",
+    "approved"
+  ],
+  [
+    "Fletcher Curtis",
+    "Architect",
+    "2021/02/22",
+    "rejected"
+  ],
+  [
+    "Bevis Pollard",
+    "Jr Adv",
+    "2021/11/14",
+    "approved"
+  ],
+  [
+    "Dale Villarreal",
+    "Jr Adv",
+    "2022/02/28",
+    "pending"
+  ],
+  [
+    "Christopher Larsen",
+    "Jr Adv",
+    "2021/03/12",
+    "approved"
+  ],
+  [
+    "Bruno Kline",
+    "SSr",
+    "2021/11/19",
+    "rejected"
+  ],
+  [
+    "Gary Parker",
+    "Designer",
+    "2021/10/03",
+    "approved"
+  ],
+  [
+    "Zahir Sparks",
+    "Designer",
+    "2021/11/30",
+    "pending"
+  ],
+  [
+    "Zachery Koch",
+    "Architect",
+    "2021/09/23",
+    "pending"
+  ],
+  [
+    "Ralph Barlow",
+    "Architect",
+    "2021/12/24",
+    "approved"
+  ],
+  [
+    "Kamal Lester",
+    "Jr",
+    "2021/12/10",
+    "pending"
+  ],
+  [
+    "Jarrod Stephens",
+    "Jr Adv",
+    "2020/08/11",
+    "rejected"
+  ]
+]
+
+export default data;
